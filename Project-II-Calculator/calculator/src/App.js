0cf8c560904476217ba52fe5873d8a1a05c060c6
raberin <rmvaberin@gmail.com>
2019-02-28 21:17:58 -0800

    Completed CalculatorDisplay JSX and CSS

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
+
 const App = () => {
   return (
-    <div>
+    <div className="App">
+    <CalculatorDisplay />
+
+
 
     </div>
   );
